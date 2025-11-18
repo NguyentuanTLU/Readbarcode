884a551a15c8ff7447619a2dfcec7c26febc3fc1
NguyentuanTLU <99778670+NguyentuanTLU@users.noreply.github.com>
2025-11-18 20:00:08 +0700

    Add files via upload

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBoxROI = new System.Windows.Forms.ListBox();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.btnDeleteROI = new System.Windows.Forms.Button();
+            this.btnSaveROI = new System.Windows.Forms.Button();
+            this.btnLoadROI = new System.Windows.Forms.Button();
+            this.btnReadBarcode = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBoxAlgorithm = new System.Windows.Forms.ComboBox();
+            this.comboBoxBarcodeFormat = new System.Windows.Forms.ComboBox();
+            this.textBoxBarcodeLength = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(940, 539);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBoxROI
+            // 
+            this.listBoxROI.FormattingEnabled = true;
+            this.listBoxROI.ItemHeight = 20;
+            this.listBoxROI.Location = new System.Drawing.Point(975, 12);
+            this.listBoxROI.Name = "listBoxROI";
+            this.listBoxROI.Size = new System.Drawing.Size(265, 364);
+            this.listBoxROI.TabIndex = 1;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(12, 604);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(111, 60);
+            this.btnLoadImage.TabIndex = 2;
+            this.btnLoadImage.Text = "LoadIMG";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // btnDeleteROI
+            // 
+            this.btnDeleteROI.Location = new System.Drawing.Point(172, 604);
+            this.btnDeleteROI.Name = "btnDeleteROI";
+            this.btnDeleteROI.Size = new System.Drawing.Size(111, 60);
+            this.btnDeleteROI.TabIndex = 3;
+            this.btnDeleteROI.Text = "Delete";
+            this.btnDeleteROI.UseVisualStyleBackColor = true;
+            this.btnDeleteROI.Click += new System.EventHandler(this.btnDeleteROI_Click_1);
+            // 
+            // btnSaveROI
+            // 
+            this.btnSaveROI.Location = new System.Drawing.Point(337, 604);
+            this.btnSaveROI.Name = "btnSaveROI";
+            this.btnSaveROI.Size = new System.Drawing.Size(111, 60);
+            this.btnSaveROI.TabIndex = 4;
+            this.btnSaveROI.Text = "SaveROI";
+            this.btnSaveROI.UseVisualStyleBackColor = true;
+            this.btnSaveROI.Click += new System.EventHandler(this.btnSaveROI_Click_1);
+            // 
+            // btnLoadROI
+            // 
+            this.btnLoadROI.Location = new System.Drawing.Point(501, 604);
+            this.btnLoadROI.Name = "btnLoadROI";
+            this.btnLoadROI.Size = new System.Drawing.Size(111, 60);
+            this.btnLoadROI.TabIndex = 5;
+            this.btnLoadROI.Text = "LoadROI";
+            this.btnLoadROI.UseVisualStyleBackColor = true;
+            this.btnLoadROI.Click += new System.EventHandler(this.btnLoadROI_Click_1);
+            // 
+            // btnReadBarcode
+            // 
+            this.btnReadBarcode.Location = new System.Drawing.Point(667, 604);
+            this.btnReadBarcode.Name = "btnReadBarcode";
+            this.btnReadBarcode.Size = new System.Drawing.Size(111, 60);
+            this.btnReadBarcode.TabIndex = 6;
+            this.btnReadBarcode.Text = "Read";
+            this.btnReadBarcode.UseVisualStyleBackColor = true;
+            this.btnReadBarcode.Click += new System.EventHandler(this.btnReadBarcode_Click_1);
+            // 
+            // comboBoxAlgorithm
+            // 
+            this.comboBoxAlgorithm.FormattingEnabled = true;
+            this.comboBoxAlgorithm.Location = new System.Drawing.Point(975, 397);
+            this.comboBoxAlgorithm.Name = "comboBoxAlgorithm";
+            this.comboBoxAlgorithm.Size = new System.Drawing.Size(265, 28);
+            this.comboBoxAlgorithm.TabIndex = 7;
+            // 
+            // comboBoxBarcodeFormat
+            // 
+            this.comboBoxBarcodeFormat.FormattingEnabled = true;
+            this.comboBoxBarcodeFormat.Location = new System.Drawing.Point(975, 464);
+            this.comboBoxBarcodeFormat.Name = "comboBoxBarcodeFormat";
+            this.comboBoxBarcodeFormat.Size = new System.Drawing.Size(265, 28);
+            this.comboBoxBarcodeFormat.TabIndex = 8;
+            // 
+            // textBoxBarcodeLength
+            // 
+            this.textBoxBarcodeLength.Location = new System.Drawing.Point(975, 524);
+            this.textBoxBarcodeLength.Name = "textBoxBarcodeLength";
+            this.textBoxBarcodeLength.Size = new System.Drawing.Size(265, 26);
+            this.textBoxBarcodeLength.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 731);
+            this.Controls.Add(this.textBoxBarcodeLength);
+            this.Controls.Add(this.comboBoxBarcodeFormat);
+            this.Controls.Add(this.comboBoxAlgorithm);
+            this.Controls.Add(this.btnReadBarcode);
+            this.Controls.Add(this.btnLoadROI);
+            this.Controls.Add(this.btnSaveROI);
+            this.Controls.Add(this.btnDeleteROI);
+            this.Controls.Add(this.btnLoadImage);
+            this.Controls.Add(this.listBoxROI);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBoxROI;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnDeleteROI;
+        private System.Windows.Forms.Button btnSaveROI;
+        private System.Windows.Forms.Button btnLoadROI;
+        private System.Windows.Forms.Button btnReadBarcode;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBoxAlgorithm;
+        private System.Windows.Forms.ComboBox comboBoxBarcodeFormat;
+        private System.Windows.Forms.TextBox textBoxBarcodeLength;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using ZXing;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap loadedImage;
+        private bool drawing = false;
+        private Point startPoint;
+        private Rectangle currentROI = Rectangle.Empty;
+        private List<ROI> rois = new List<ROI>();
+        private int roiCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Thuật toán
+            comboBoxAlgorithm.Items.AddRange(new string[] { "Barcode", "HSV", "Template Matching" });
+            comboBoxAlgorithm.SelectedIndexChanged += ComboBoxAlgorithm_SelectedIndexChanged;
+
+            // Barcode format
+            comboBoxBarcodeFormat.Items.AddRange(new string[]
+            {
+                "AUTO", "CODE_128", "CODE_39", "EAN_13", "EAN_8", "QR_CODE"
+            });
+            comboBoxBarcodeFormat.SelectedIndex = 0;
+
+            // PictureBox events
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.Paint += PictureBox1_Paint;
+
+            // ROI selection
+            listBoxROI.SelectedIndexChanged += ListBoxROI_SelectedIndexChanged;
+
+            // Ẩn controls barcode mặc định
+            comboBoxBarcodeFormat.Visible = false;
+            textBoxBarcodeLength.Visible = false;
+            btnReadBarcode.Visible = false;
+        }
+
+        // Load ảnh
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.png;*.jpg;*.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                loadedImage = new Bitmap(ofd.FileName);
+                pictureBox1.Image = loadedImage;
+            }
+        }
+
+        // Vẽ ROI
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (loadedImage == null) return;
+            drawing = true;
+            startPoint = e.Location;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                int x = Math.Min(startPoint.X, e.X);
+                int y = Math.Min(startPoint.Y, e.Y);
+                int w = Math.Abs(startPoint.X - e.X);
+                int h = Math.Abs(startPoint.Y - e.Y);
+                currentROI = new Rectangle(x, y, w, h);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+                roiCounter++;
+                ROI roi = new ROI
+                {
+                    id = roiCounter,
+                    x = currentROI.X,
+                    y = currentROI.Y,
+                    w = currentROI.Width,
+                    h = currentROI.Height
+                };
+                rois.Add(roi);
+                listBoxROI.Items.Add($"ROI {roi.id}");
+                currentROI = Rectangle.Empty;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentROI != Rectangle.Empty)
+                e.Graphics.DrawRectangle(Pens.Red, currentROI);
+
+            foreach (var roi in rois)
+            {
+                e.Graphics.DrawRectangle(Pens.Blue, roi.x, roi.y, roi.w, roi.h);
+            }
+        }
+
+        // Xóa ROI
+        private void btnDeleteROI_Click_1(object sender, EventArgs e)
+        {
+            if (listBoxROI.SelectedIndex >= 0)
+            {
+                int index = listBoxROI.SelectedIndex;
+                rois.RemoveAt(index);
+                listBoxROI.Items.RemoveAt(index);
+                pictureBox1.Invalidate();
+            }
+        }
+
+
+        // Lưu ROI vào JSON
+        private void btnSaveROI_Click_1(object sender, EventArgs e)
+        {
+            if (rois.Count == 0) return;
+            string json = JsonConvert.SerializeObject(rois, Formatting.Indented);
+            File.WriteAllText("rois.json", json);
+            MessageBox.Show("Đã lưu ROI vào rois.json");
+        }
+
+        // Load ROI từ JSON
+        private void btnLoadROI_Click_1(object sender, EventArgs e)
+        {
+            if (!File.Exists("rois.json")) return;
+            string json = File.ReadAllText("rois.json");
+            rois = JsonConvert.DeserializeObject<List<ROI>>(json);
+            listBoxROI.Items.Clear();
+            roiCounter = 0;
+            foreach (var roi in rois)
+            {
+                roiCounter = Math.Max(roiCounter, roi.id);
+                listBoxROI.Items.Add($"ROI {roi.id}");
+            }
+            pictureBox1.Invalidate();
+        }
+
+
+        // Thay đổi thuật toán
+        private void ComboBoxAlgorithm_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool isBarcode = comboBoxAlgorithm.SelectedItem.ToString() == "Barcode";
+            comboBoxBarcodeFormat.Visible = isBarcode;
+            textBoxBarcodeLength.Visible = isBarcode;
+            btnReadBarcode.Visible = isBarcode;
+        }
+
+        private void ListBoxROI_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        // Đọc Barcode
+        private void btnReadBarcode_Click_1(object sender, EventArgs e)
+        {
+            if (listBoxROI.SelectedIndex < 0)
+            {
+                MessageBox.Show("Vui lòng chọn ROI!");
+                return;
+            }
+            if (comboBoxAlgorithm.SelectedItem?.ToString() != "Barcode")
+            {
+                MessageBox.Show("Vui lòng chọn thuật toán Barcode!");
+                return;
+            }
+
+            ROI roi = rois[listBoxROI.SelectedIndex];
+
+            Bitmap roiBmp = new Bitmap(roi.w, roi.h);
+            using (Graphics g = Graphics.FromImage(roiBmp))
+            {
+                g.DrawImage(loadedImage, 0, 0, new Rectangle(roi.x, roi.y, roi.w, roi.h), GraphicsUnit.Pixel);
+            }
+
+            BarcodeReader reader = new BarcodeReader();
+            if (comboBoxBarcodeFormat.SelectedItem.ToString() != "AUTO")
+            {
+                reader.Options.PossibleFormats = new List<BarcodeFormat> { MapBarcodeFormat(comboBoxBarcodeFormat.SelectedItem.ToString()) };
+            }
+
+            var result = reader.Decode(roiBmp);
+            if (result != null)
+            {
+                // Kiểm tra độ dài nếu nhập
+                if (int.TryParse(textBoxBarcodeLength.Text, out int length))
+                {
+                    if (result.Text.Length != length)
+                    {
+                        MessageBox.Show($"Barcode đọc được: {result.Text}\n(Không đúng độ dài {length})", "Kết quả Barcode");
+                        return;
+                    }
+                }
+                MessageBox.Show($"Barcode đọc được: {result.Text}", "Kết quả Barcode");
+            }
+            else
+            {
+                MessageBox.Show("Không đọc được barcode", "Kết quả Barcode");
+            }
+        }
+
+
+        private BarcodeFormat MapBarcodeFormat(string format)
+        {
+            switch (format)
+            {
+                case "CODE_128":
+                    return BarcodeFormat.CODE_128;
+                case "CODE_39":
+                    return BarcodeFormat.CODE_39;
+                case "EAN_13":
+                    return BarcodeFormat.EAN_13;
+                case "EAN_8":
+                    return BarcodeFormat.EAN_8;
+                case "QR_CODE":
+                    return BarcodeFormat.QR_CODE;
+                default:
+                    return BarcodeFormat.CODE_128;
+            }
+        }
+    }
+
+    public class ROI
+    {
+        public int id { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int w { get; set; }
+        public int h { get; set; }
+    }
+}
